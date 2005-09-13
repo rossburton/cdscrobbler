6a523b5cfea981c146b5cea0c4c9cd4a782c38b5
Ross Burton <ross@burtonini.com>
2005-09-13 15:29:53 +0000

    Initial import

---
+++ scrobbler.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+import urllib2,urllib,re,time,md5,datetime,sys
+
+class Scrobbler(object):
+    def __init__(self,user,password,client="tst",version="1.0",url="http://post.audioscrobbler.com/"):
+        self.url = url
+        self.user = user
+        self.password = password
+        self.client = client
+        self.version = version
+    def handshake(self):
+        url = self.url+"?"+urllib.urlencode({
+            "hs":"true",
+            "p":"1.1",
+            "c":self.client,
+            "v":self.version,
+            "u":self.user
+            })
+        result = urllib2.urlopen(url).readlines()
+        if result[0].startswith("BADUSER"):
+            return self.baduser(result[1:])
+        if result[0].startswith("UPTODATE"):
+            return self.uptodate(result[1:])
+        if result[0].startswith("FAILED"):
+            return self.failed(result)
+    def uptodate(self,lines):
+        self.md5 = re.sub("\n$","",lines[0])
+        self.submiturl = re.sub("\n$","",lines[1])
+        self.interval(lines[2])
+    def baduser(self,lines):
+        print "Bad user"
+    def failed(self,lines):
+        print lines[0]
+        self.interval(lines[1])
+    def interval(self,line):
+        match = re.match("INTERVAL (\d+)",line)
+        if match is not None:
+            print "[audioscrobbler] Sleeping for",match.group(1),"secs"
+            time.sleep(int(match.group(1)))
+    def submit(self,tracks):
+        print "[audioscrobbler] Submitting"
+        md5response = md5.md5(md5.md5(self.password).hexdigest()+self.md5).hexdigest()
+        post = "u="+self.user+"&s="+md5response
+        count = 0
+        for track in tracks:
+            post += "&"
+            post += track.urlencoded(count)
+            count += 1
+        post = unicode(post)
+        print post
+        result = urllib2.urlopen(self.submiturl,post)
+        results = result.readlines()
+        if results[0].startswith("OK"):
+            print "OK"
+            self.interval(results[1])
+        if results[0].startswith("FAILED"):
+            self.failed([results[0],"INTERVAL 0"])
+
+class Track(object):
+    def __init__(self,artist,name,album,length,mbid=None,tracktime=None):
+        self.params = {}
+        self.artist = artist
+        self.name = name
+        self.album = album
+        self.length = length
+        self.mbid = mbid
+        if tracktime:
+            self.tracktime = tracktime
+        else:
+            self.tracktime = datetime.datetime.now().isoformat(' ')
+
+    def __repr__(self):
+        return "'"+self.name+"' by '"+self.artist+"' from '"+self.album+"'"
+
+    def urlencoded(self,num):
+        encode = ""
+        encode += "a["+str(num)+"]="+urllib.quote_plus(self.artist)
+        encode += "&t["+str(num)+"]="+urllib.quote_plus(self.name)
+        encode += "&l["+str(num)+"]="+urllib.quote_plus(str(self.length))
+        encode += "&i["+str(num)+"]="+ urllib.quote_plus(self.tracktime.isoformat(' '))
+        if self.mbid is not None:
+            encode += "&m["+str(num)+"]="+urllib.quote_plus(self.mbid)
+        else:
+            encode += "&m["+str(num)+"]="
+        encode += "&b["+str(num)+"]="+urllib.quote_plus(self.album)
+        return encode
---
+++ upload.py
@@ -0,0 +1,67 @@
+#! /usr/bin/python
+
+# Copyright (C) 2005 Ross Burton <ross@burtonini.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+import sys, datetime
+
+import musicbrainz
+from musicbrainz.queries import *
+
+import scrobbler
+
+mb = musicbrainz.mb()
+
+if len(sys.argv) == 2:
+    mb.SetDepth(4)
+    mb.QueryWithArgs(MBQ_GetAlbumById, [sys.argv[1]])
+else:
+    mb.SetDepth(2)
+    mb.Query(MBQ_GetCDTOC)
+    cdid = mb.GetResultData(MBE_TOCGetCDIndexId)
+    mb.QueryWithArgs(MBQ_GetCDInfoFromCDIndexId, [cdid])
+
+count = mb.GetResultInt(MBE_GetNumAlbums)
+if count == 0:
+    print "Found no albums"
+    sys.exit(1)
+if count > 1:
+    print "TODO: found %d albums" % count
+    sys.exit(2)
+    
+tracks = []
+
+mb.Select1(MBS_SelectAlbum, 1)
+album = mb.GetResultData(MBE_AlbumGetAlbumName)
+for ii in range(1, mb.GetResultInt(MBE_AlbumGetNumTracks) + 1):
+    mbid = mb.GetIDFromURL(mb.GetResultData1(MBE_AlbumGetTrackId, ii))
+    artist = mb.GetResultData1(MBE_AlbumGetArtistName, ii)
+    name = mb.GetResultData1(MBE_AlbumGetTrackName, ii)
+    dura = mb.GetResultInt1(MBE_AlbumGetTrackDuration, ii) / 1000
+    track = mb.GetResultInt1(MBE_AlbumGetTrackNum, ii)
+    tracks.append(scrobbler.Track(artist, name, album, dura, mbid, None))
+
+# Fix up times on tracks
+current = datetime.datetime.now()
+tracks.reverse()
+for t in tracks:
+    current = current - datetime.timedelta(seconds=t.length)
+    t.tracktime = current
+tracks.reverse()
+
+scrob = scrobbler.Scrobbler("rossburton", "dsiasb")
+scrob.handshake()
+scrob.submit(tracks)
