6bd7fca426c545660e46d87b9f0950051f73a6a8
Ross Burton <ross@burtonini.com>
2005-10-23 20:27:57 +0000

    Rename upload to cdscrobber

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ setup(
     name='cdscrobbler', version='1.0',
     author='Ross Burton', author_email='ross@burtonini.com',
     py_modules=['scrobbler'],
-    scripts=['upload.py']
+    scripts=['cdscrobbler.py']
     )
