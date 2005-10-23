7da44c4bdc7403531bcc6db499b2c7a24287e8f6
Ross Burton <ross@burtonini.com>
2005-10-23 20:27:01 +0000

    Use distutils

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(
+    name='cdscrobbler', version='1.0',
+    author='Ross Burton', author_email='ross@burtonini.com',
+    py_modules=['scrobbler'],
+    scripts=['upload.py']
+    )
