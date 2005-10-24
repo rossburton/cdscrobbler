08b65df55aba2302811992ee3569a3d2de17c939
Ross Burton <ross@burtonini.com>
2005-10-24 09:13:02 +0000

    Add URL

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from distutils.core import setup
 setup(
     name='cdscrobbler', version='1.0',
     author='Ross Burton', author_email='ross@burtonini.com',
+    url='http://www.burtonini.com/',
     py_modules=['scrobbler'],
     scripts=['cdscrobbler.py']
     )
