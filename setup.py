19110ef58979c05b7fa6304c2adaed4fbb79b6d1
Ross Burton <ross@burtonini.com>
2005-10-24 09:11:20 +0000

    Add check for musicbrainz module

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+try:
+    import musicbrainz
+except ImportError:
+    print "\nWARNING: cannot find MusicBrainz module.  cdscrobbler will not work without it.\n"
+
 from distutils.core import setup
 setup(
     name='cdscrobbler', version='1.0',
